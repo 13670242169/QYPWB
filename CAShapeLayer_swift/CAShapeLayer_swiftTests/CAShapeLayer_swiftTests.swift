604cfdddc5e7166dbf16f362ee2602472fbf2a36
Apple <980471259@qq.com>
2017-10-12 17:53:34 +0800

    创建一个swift的CAShapeLayer学习项目

---
+++ CAShapeLayer_swift/CAShapeLayer_swiftTests/CAShapeLayer_swiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  CAShapeLayer_swiftTests.swift
+//  CAShapeLayer_swiftTests
+//
+//  Created by Apple on 2017/10/12.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+import XCTest
+@testable import CAShapeLayer_swift
+
+class CAShapeLayer_swiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
